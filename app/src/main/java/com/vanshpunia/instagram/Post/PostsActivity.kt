b167f5696a9389c4e0871977f4e1917d19232e29
Vansh Punia <vanshpunia007@gmial.com>
2024-04-19 01:31:54 +0530

    profile working

---
+++ app/src/main/java/com/vanshpunia/instagram/Post/PostsActivity.kt
@@ -0,0 +1,12 @@
+package com.vanshpunia.instagram.Post
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vanshpunia.instagram.R
+
+class PostsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posts)
+    }
+}
---
+++ app/src/main/java/com/vanshpunia/instagram/Post/ReelsActivity.kt
@@ -0,0 +1,12 @@
+package com.vanshpunia.instagram.Post
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vanshpunia.instagram.R
+
+class ReelsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reels)
+    }
+}
